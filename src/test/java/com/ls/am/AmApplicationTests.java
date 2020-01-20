4cf7f153e6940a876ec63803926a8f2a5d9c67a8
DESKTOP-SQJG28P\EDZ <991072355@qq.com>
2020-01-20 18:04:48 +0800

    新建项目

---
+++ src/test/java/com/ls/am/AmApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ls.am;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AmApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
